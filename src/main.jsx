735c71daa644b70ac32ed4be319000f9ce64a40f
Ahmed Ali <ahmedalielian20@gmail.com>
2024-05-15 10:01:23 +0300

    first upload

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,14 @@ import './index.css'
 import'@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </HashRouter>
+
 )
